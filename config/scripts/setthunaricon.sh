6e1ef84341ea005309edf02574d8b61848d15931
qoijjj <129108030+qoijjj@users.noreply.github.com>
2023-12-30 21:13:00 -0800

    fix thunar foot integration

--- config/scripts/setthunaricon.sh
+++ config/scripts/setthunaricon.sh
@@ -4,3 +4,4 @@
 set -oue pipefail
 
 sed -i 's/Icon=org.xfce.thunar/Icon=system-file-manager/' /usr/share/applications/thunar.desktop
+sed -i 's/<command>exo-open --working-directory %f --launch TerminalEmulator</command>/<command>foot -W %f</command>/' /usr/etc/xdg/Thunar/uca.xml
