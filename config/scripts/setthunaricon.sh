749e509abf29832377af8cb0bc58bfc6e61f6490
qoijjj <129108030+qoijjj@users.noreply.github.com>
2023-12-30 21:22:35 -0800

    Update setthunaricon.sh

--- config/scripts/setthunaricon.sh
+++ config/scripts/setthunaricon.sh
@@ -4,4 +4,4 @@
 set -oue pipefail
 
 sed -i 's/Icon=org.xfce.thunar/Icon=system-file-manager/' /usr/share/applications/thunar.desktop
-sed -i 's/<command>exo-open --working-directory %f --launch TerminalEmulator</command>/<command>foot -W %f</command>/' /usr/etc/xdg/Thunar/uca.xml
+sed -i 's/exo-open --working-directory %f --launch TerminalEmulator/foot -W %f/' /usr/etc/xdg/Thunar/uca.xml
