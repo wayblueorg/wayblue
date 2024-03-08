abac92292042bfdbf61a6095cc261de5a9805dd2
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-03-07 22:30:32 -0800

    chore: theming improvements

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -3,8 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' 
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'breeze-dark'
-
 sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
