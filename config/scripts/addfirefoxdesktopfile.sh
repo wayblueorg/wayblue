f6974c01383d03e35fd5ef32c1a91e415e731344
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-03-07 19:33:33 -0800

    feat: add firefox launcher to wf-shell panel

---
+++ config/scripts/addfirefoxdesktopfile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+sed -i 's/launcher_thunar = thunar.desktop/launcher_chromium = firefox.desktop\nlauncher_thunar = thunar.desktop/' /usr/share/wayfire/wf-shell.ini 
