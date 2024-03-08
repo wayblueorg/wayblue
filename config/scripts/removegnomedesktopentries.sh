b10a4fe46a077f04029f2048b9f9c48bcab5c516
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-03-07 20:03:12 -0800

    chore: additional gdm cleanup

--- config/scripts/removegnomedesktopentries.sh
+++ config/scripts/removegnomedesktopentries.sh
@@ -6,3 +6,13 @@ set -oue pipefail
 rm /usr/share/xsessions/*
 rm /usr/share/wayland-sessions/gnome.desktop
 rm /usr/share/wayland-sessions/gnome-wayland.desktop
+rm /usr/share/applications/gnome-*.desktop
+rm /usr/share/applications/org.gnome.ColorProfileViewer.desktop
+rm /usr/share/applications/org.gnome.Evolution-alarm-notify.desktop
+rm /usr/share/applications/org.gnome.evolution-data-server.OAuth2-handler.desktop
+rm /usr/share/applications/org.gnome.Settings.desktop
+rm /usr/share/applications/org.gnome.Shell.desktop
+rm /usr/share/applications/org.gnome.Shell.Extensions.desktop
+rm /usr/share/applications/org.gnome.Shell.PortalHelper.desktop
+rm /usr/share/applications/org.gnome.Tecla.desktop
+rm /usr/share/applications/org.gnome.Tour.desktop
