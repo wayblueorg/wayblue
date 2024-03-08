261232e2474183d1f26732da64bc6ecfda7fcb5d
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-03-07 21:14:20 -0800

    fix: gdm startup

--- config/scripts/removegnomedesktopentries.sh
+++ config/scripts/removegnomedesktopentries.sh
@@ -3,9 +3,11 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rm /usr/share/xsessions/*
-rm /usr/share/wayland-sessions/gnome.desktop
-rm /usr/share/wayland-sessions/gnome-wayland.desktop
+mv /usr/share/xsessions/gnome.desktop /usr/share/xsessions/99-gnome.desktop 
+mv /usr/share/xsessions/gnome-xorg.desktop /usr/share/xsessions/99-gnome-xorg.desktop 
+mv /usr/share/wayland-sessions/gnome.desktop /usr/share/wayland-sessions/99-gnome.desktop
+mv /usr/share/wayland-sessions/gnome-wayland.desktop /usr/share/wayland-sessions/99-gnome-wayland.desktop
+rm /usr/share/applications/gnome-*.desktop
 rm /usr/share/applications/org.gnome.ColorProfileViewer.desktop
 rm /usr/share/applications/org.gnome.Evolution-alarm-notify.desktop
 rm /usr/share/applications/org.gnome.evolution-data-server.OAuth2-handler.desktop
