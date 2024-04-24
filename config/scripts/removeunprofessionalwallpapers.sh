82443c9b45a19eb282cdf8eee6ff5f664624cb54
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-04-23 22:30:08 -0700

    feat: add hyprland modules to waybar for hyprland

--- config/scripts/removeunprofessionalwallpapers.sh
+++ config/scripts/removeunprofessionalwallpapers.sh
@@ -4,7 +4,7 @@
 set -oue pipefail
 
 for file in /usr/share/hyprland/*.png; do
-    if [[ $file == *anime*.png ]]; then
+    if [[ $file == *anime*.png || $file == wall2.png ]]; then
         rm "$file"
         echo "Removed: $file"
     fi
---
+++ config/scripts/sethyprlandwaybarmodules.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+sed -i 's/"sway/workspaces"/"hyprland/workspaces""/' /usr/etc/xdg/waybar/config
+sed -i 's/"sway/mode"/"hyprland/submap""/' /usr/etc/xdg/waybar/config
+sed -i 's/"sway/window"/"hyprland/window""/' /usr/etc/xdg/waybar/config
+sed -i 's/"sway/language"/"hyprland/language""/' /usr/etc/xdg/waybar/config
