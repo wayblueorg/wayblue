7ce5ee6c818e242117060661847b1325e7b6f8ad
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-04-23 22:53:22 -0700

    fix: escape slashes for sed

--- config/scripts/sethyprlandwaybarmodules.sh
+++ config/scripts/sethyprlandwaybarmodules.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/"sway/workspaces"/"hyprland/workspaces"/' /usr/etc/xdg/waybar/config
-sed -i 's/"sway/mode"/"hyprland/submap"/' /usr/etc/xdg/waybar/config
-sed -i 's/"sway/window"/"hyprland/window"/' /usr/etc/xdg/waybar/config
-sed -i 's/"sway/language"/"hyprland/language"/' /usr/etc/xdg/waybar/config
+sed -i 's/sway\/workspaces/hyprland\/workspaces/' /usr/etc/xdg/waybar/config
+sed -i 's/sway\/mode/hyprland\/submap/' /usr/etc/xdg/waybar/config
+sed -i 's/sway\/window/hyprland\/window/' /usr/etc/xdg/waybar/config
+sed -i 's/sway\/language/hyprland\/language/' /usr/etc/xdg/waybar/config
