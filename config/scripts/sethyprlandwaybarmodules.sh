2dd2bf907bbde8f7bc15af0931c220e2a1d6201f
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-04-23 23:28:37 -0700

    fix: remove waybar language as it takes significant space with minimal informational value

--- config/scripts/sethyprlandwaybarmodules.sh
+++ config/scripts/sethyprlandwaybarmodules.sh
@@ -6,4 +6,3 @@ set -oue pipefail
 sed -i 's/sway\/workspaces/hyprland\/workspaces/' /usr/etc/xdg/waybar/config
 sed -i 's/sway\/mode/hyprland\/submap/' /usr/etc/xdg/waybar/config
 sed -i 's/sway\/window/hyprland\/window/' /usr/etc/xdg/waybar/config
-sed -i 's/sway\/language/hyprland\/language/' /usr/etc/xdg/waybar/config
