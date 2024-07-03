1aba1681abb450f00444fa657fd01de3c075b94c
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-07-03 14:02:58 -0700

    fix: icon theme name

--- config/files/usr/etc/profile.d/settheming.sh
+++ config/files/usr/etc/profile.d/settheming.sh
@@ -1,3 +1,3 @@
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'ePapirus'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme 
+sed -i 's/Inherits=Adwaita/Inherits=Papirus/' /usr/share/icons/default/index.theme 
