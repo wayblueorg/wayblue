ca567e8f51e2feaee93d5713ff2218594d015038
tleydxdy <shironeko.github@tesaguri.club>
2024-07-03 22:11:34 -0400

    update waybar config and fix for river (#49)
    * chore: sync with upstream waybar config
    
    * fix: river waybar config

--- config/scripts/sethyprlandwaybarmodules.sh
+++ config/scripts/sethyprlandwaybarmodules.sh
@@ -3,6 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/sway\/workspaces/hyprland\/workspaces/' /usr/etc/xdg/waybar/config
-sed -i 's/sway\/mode/hyprland\/submap/' /usr/etc/xdg/waybar/config
-sed -i 's/sway\/window/hyprland\/window/' /usr/etc/xdg/waybar/config
+sed -i 's/sway\/workspaces/hyprland\/workspaces/' /usr/etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/mode/hyprland\/submap/' /usr/etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/window/hyprland\/window/' /usr/etc/xdg/waybar/config.jsonc
