66ab7a412ca76327e40e0d5ddc3b8d575ddd689f
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-07-05 14:49:14 -0700

    fix: removing icon theme setting to prevent breaking steam installation

--- config/scripts/settheming.sh
+++ config/scripts/settheming.sh
@@ -3,4 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/Inherits=Adwaita/Inherits=Papirus/' /usr/share/icons/default/index.theme 
+# Removing this for now since it breaks steam layering.
+# https://github.com/ublue-os/bluefin/issues/1258
+# sed -i 's/Inherits=Adwaita/Inherits=Papirus/' /usr/share/icons/default/index.theme 
