ad769f5400b7f702e6fae5a35dbc49c409d97580
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-08-08 15:20:15 -0700

    chore: move /usr/etc to /etc per upstream rpm-ostree recommendation (#53)

--- files/scripts/setriverwaybarmodules.sh
+++ files/scripts/setriverwaybarmodules.sh
@@ -3,6 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/sway\/workspaces/river\/tags/' /usr/etc/xdg/waybar/config.jsonc
-sed -i 's/sway\/mode/river\/mode/' /usr/etc/xdg/waybar/config.jsonc
-sed -i 's/sway\/window/river\/window/' /usr/etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/workspaces/river\/tags/' /etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/mode/river\/mode/' /etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/window/river\/window/' /etc/xdg/waybar/config.jsonc
