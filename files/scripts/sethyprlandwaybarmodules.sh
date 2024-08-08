aba6c777f904e26d53ae269be08e38a23fb3e444
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-08-08 13:40:26 -0700

    chore: move /usr/etc to /etc per upstream rpm-ostree recommendation

--- files/scripts/sethyprlandwaybarmodules.sh
+++ files/scripts/sethyprlandwaybarmodules.sh
@@ -3,6 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/sway\/workspaces/hyprland\/workspaces/' /usr/etc/xdg/waybar/config.jsonc
-sed -i 's/sway\/mode/hyprland\/submap/' /usr/etc/xdg/waybar/config.jsonc
-sed -i 's/sway\/window/hyprland\/window/' /usr/etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/workspaces/hyprland\/workspaces/' /etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/mode/hyprland\/submap/' /etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/window/hyprland\/window/' /etc/xdg/waybar/config.jsonc
