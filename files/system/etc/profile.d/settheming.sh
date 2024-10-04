1bbbfed381ca7dca1119ef8b754a48f3fce6101d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-10-04 19:16:26 +0200

    chore: Use gschema override instead of profile.d script for default theming settings (#56)

--- files/system/etc/profile.d/settheming.sh
+++
@@ -1,3 +0,0 @@
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' 
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
