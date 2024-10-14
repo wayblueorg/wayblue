ac887101e543be059113c93b457012fc0a36b32d
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-13 20:52:08 -0700

    fix script

--- files/scripts/installandroidudev.sh
+++ files/scripts/installandroidudev.sh
@@ -3,6 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${RELEASE}"/ublue-os-staging-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${OS_VERSION}"/ublue-os-staging-fedora-"${OS_VERSION}".repo
 rpm-ostree install android-udev-rules
 rm /etc/yum.repos.d/_copr_ublue-os_staging.repo
