1b7091ed9e0a31e8705c070e07c0aa712430488a
Markbeep <mrkswrn@gmail.com>
2025-08-12 15:23:51 +0200

    fix: use niri/workspaces module instead of niri/tags

--- files/scripts/setniriwaybarmodules.sh
+++ files/scripts/setniriwaybarmodules.sh
@@ -3,6 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/sway\/workspaces/niri\/tags/' /etc/xdg/waybar/config.jsonc
-sed -i 's/sway\/mode/niri\/mode/' /etc/xdg/waybar/config.jsonc
+sed -i 's/sway\/workspaces/niri\/workspaces/' /etc/xdg/waybar/config.jsonc
 sed -i 's/sway\/window/niri\/window/' /etc/xdg/waybar/config.jsonc
