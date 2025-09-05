93853fb0488847ef3408bb9646e34b4795d9e966
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-09-04 20:57:42 -0700

    hardcoded mirror

--- files/scripts/installrpmfusion.sh
+++ files/scripts/installrpmfusion.sh
@@ -2,8 +2,8 @@
 
 set -oue pipefail
 
-RPMFUSION_FREE="https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm"
-RPMFUSION_NONFREE="https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+RPMFUSION_FREE="https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm"
+RPMFUSION_NONFREE=https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
 
 
 curl --retry 3 --retry-delay 5 -LO "${RPMFUSION_FREE}"
