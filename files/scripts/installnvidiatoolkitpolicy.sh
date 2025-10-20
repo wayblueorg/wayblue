c4e69074f099d4dfd6591664734904d2a849f149
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-20 15:41:55 -0700

    fixes

--- files/scripts/installnvidiatoolkitpolicy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
--- files/scripts/removegnomedesktopentries.sh
+++ files/scripts/removegnomedesktopentries.sh
@@ -2,7 +2,6 @@
 
 set -oue pipefail
 
-rm /usr/share/applications/org.gnome.ColorProfileViewer.desktop
 rm /usr/share/applications/org.gnome.Settings.desktop
 rm /usr/share/applications/org.gnome.Tecla.desktop
 rm /usr/share/applications/org.gnome.Tour.desktop
