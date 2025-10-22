dd342e9da84e02d3c50a92b1a86a49df53907f4c
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-21 20:28:51 -0700

    ensure negativo precedence

---
+++ files/scripts/ensurenegativopresence.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
+sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
