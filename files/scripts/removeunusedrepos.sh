588327bf1f42840cbffa54fe6b1bcf098f36294c
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-10-21 20:19:18 -0700

    fix repos

--- files/scripts/removeunusedrepos.sh
+++ files/scripts/removeunusedrepos.sh
@@ -7,4 +7,7 @@ rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 rm -f /etc/yum.repos.d/fedora-negativo17.repo
 rm -f /etc/yum.repos.d/eyecantcu-supergfxctl.repo
 rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
+rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo.rpmsave
 rm -f /etc/yum.repos.d/nvidia-container-toolkit.repo
+rm -f /etc/yum.repos.d/fedora-cisco-openh264.repo
